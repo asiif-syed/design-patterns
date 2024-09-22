da274099e26c0eb916e74f505ed3b20895a03127
Asif Syed <asiifsyed@gmail.com>
2024-09-22 15:08:24 -0700

    Builder Design pattern

---
+++ src/Builder/Client.java
@@ -0,0 +1,15 @@
+package Builder;
+
+import Singleton.Singleton;
+
+public class Client {
+    public static void main(String[] args) {
+        Student s = Student.getBuilder()
+                .setName("Asif Syed")
+                .setAge(32)
+                .setBatch("Dec 2024")
+                .setPhone("+1 (778) 268-6665")
+                .build();
+        Singleton singletonInstance = Singleton.getInstance();
+    }
+}
