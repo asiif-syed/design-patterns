804c84cd23d609f50cdfe45e7883031a362a7dce
Asif Syed <asiifsyed@gmail.com>
2024-12-25 15:46:10 -0800

    Improvements

--- LLD_Case_Studies/src/splitwise/controllers/App.java
+++ LLD_Case_Studies/src/splitwise/controllers/App.java
@@ -32,13 +32,15 @@ public class App {
         // Create user expenses
         // A : 500, B:500, C:500
         // D : -1500
-        UserExpense uE1 = new UserExpense(amir, dinnerExpense, 500, UserExpenseType.PAID_BY);
-        UserExpense uE2 = new UserExpense(shreyasi, dinnerExpense, 500, UserExpenseType.PAID_BY);
-        UserExpense uE5 = new UserExpense(tanay, dinnerExpense, 500, UserExpenseType.PAID_BY);
+        UserExpense uE1 = new UserExpense(amir, dinnerExpense, 2000, UserExpenseType.PAID_BY);
+        UserExpense uE2 = new UserExpense(shreyasi, dinnerExpense, 2500, UserExpenseType.PAID_BY);
+        UserExpense uE3 = new UserExpense(shubham, dinnerExpense, 500, UserExpenseType.PAID_BY);
+        UserExpense uE4 = new UserExpense(tanay, dinnerExpense, 0, UserExpenseType.PAID_BY);
 
-        UserExpense uE3 = new UserExpense(amir, dinnerExpense, 0, UserExpenseType.HAD_TO_PAY);
-        UserExpense uE4 = new UserExpense(shreyasi, dinnerExpense, 0, UserExpenseType.HAD_TO_PAY);
-        UserExpense uE6 = new UserExpense(shubham, dinnerExpense, 1500, UserExpenseType.HAD_TO_PAY);
+        UserExpense uE5 = new UserExpense(amir, dinnerExpense, 3000, UserExpenseType.HAD_TO_PAY);
+        UserExpense uE6 = new UserExpense(shreyasi, dinnerExpense, 1400, UserExpenseType.HAD_TO_PAY);
+        UserExpense uE7 = new UserExpense(shubham, dinnerExpense, 300, UserExpenseType.HAD_TO_PAY);
+        UserExpense uE8 = new UserExpense(tanay, dinnerExpense, 300, UserExpenseType.HAD_TO_PAY);
 
         // Add created group to the group repo
         Map<String, Group> groupMap = new HashMap<>();
@@ -46,17 +48,22 @@ public class App {
         GroupRepository groupRepository = new GroupRepository(groupMap);
 
         // Create user repo
-        UserExpenseRepository userExpenseRepository = new UserExpenseRepository(Arrays.asList(uE1, uE2, uE3, uE4, uE5, uE6));
+        UserExpenseRepository userExpenseRepository = new UserExpenseRepository(Arrays.asList(uE1, uE2, uE3, uE4, uE5, uE6, uE7, uE8));
 
         // Create user service
         UserService userService = new UserService(groupRepository, userExpenseRepository, settleUpStrategy);
 
         // Create user controller
         UserController userController = new UserController(userService);
-        List<Transaction> transactions = userController.settleUser("tanay", "GOA GUYS");
-
-        for (Transaction transaction: transactions){
-            System.out.println("From: " + transaction.getFrom() + ", To: " + transaction.getTo() + ", Amount: "  + transaction.getAmount());
-        }
+        List<Transaction> groupTransactions = userController.settleGroup("GOA GUYS");
+        userController.printUserTransactions(groupTransactions);
+//        List<Transaction> tanayTransactions = userController.settleUser("tanay", "GOA GUYS");
+//        userController.printUserTransactions(tanayTransactions);
+//        List<Transaction> shreyasiTransactions = userController.settleUser("Shreyasi", "GOA GUYS");
+//        userController.printUserTransactions(shreyasiTransactions);
+//        List<Transaction> amirTransactions = userController.settleUser("Amir", "GOA GUYS");
+//        userController.printUserTransactions(amirTransactions);
+//        List<Transaction> shubhamTransactions = userController.settleUser("Shubham", "GOA GUYS");
+//        userController.printUserTransactions(shubhamTransactions);
     }
 }
--- LLD_Case_Studies/src/splitwise/controllers/UserController.java
+++ LLD_Case_Studies/src/splitwise/controllers/UserController.java
@@ -15,4 +15,12 @@ public class UserController {
     public List<Transaction> settleUser(String username, String groupName){
         return userService.settleUser(username, groupName);
     }
+    public List<Transaction> settleGroup(String groupName){
+        return userService.settleGroup(groupName);
+    }
+    public void printUserTransactions(List<Transaction> transactions){
+        for (Transaction transaction: transactions){
+            System.out.println("From: " + transaction.getFrom() + ", To: " + transaction.getTo() + ", Amount: "  + transaction.getAmount());
+        }
+    }
 }
